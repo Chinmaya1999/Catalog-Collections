ad1751c7b55beb9af2bad1eeebc20f9d1b0e1039
Chinmaya Kumar Mallick <chinmayakumarmallick@Chinmayas-MacBook-Air.local>
2026-08-06 21:19:46 +0530

    new update

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://api.adihuman.com', 'http://localhost:3000', 'http://localhost:5002'],
+  origin: ['https://catalog.adihuman.com', 'http://localhost:3000', 'http://localhost:5002', 'https://api.adihuman.com'],
   credentials: true
 }));
 app.use(express.json());
